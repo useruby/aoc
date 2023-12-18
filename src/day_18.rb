8e4a93a3d401de250294e875954f0b61dc9ea760
Aleksei Artamonov <i@useruby.com>
2023-12-18 20:25:40 +0100

    Improves Day 18

--- src/day_18.rb
+++ src/day_18.rb
@@ -12,53 +12,46 @@ class Day18 < Day
     attr_reader :path
 
     def initialize
-      @digger = [0, 0]
-      @path = [@digger]
+      @path = [[0, 0]]
       @directions = DIRECTIONS.to_h.freeze
     end
 
     def digging(steps, direction)
-      velocity = @directions[direction]
-
-      @path << @digger = [
-        @digger.first + (velocity.first * steps),
-        @digger.last + (velocity.last * steps)
-      ]
+      @path << @path.last.zip(@directions[direction]).map { |position, velocity| position + (steps * velocity) }
     end
   end
 
   def result
-    digging_sequence =
-      if enable_part_two
-        @input.map do |line|
-          _, _, hex_number = line.split
-          match = hex_number.match(/\(\#(?<steps>[0-9a-f]{5})(?<direction>[0-9a-f]{1})\)/)
-          [Lagoon::DIRECTIONS[match[:direction].to_i(16)].first, match[:steps].to_i(16)]
-        end
-      else
-        @input.map do |line|
-          direction, steps, = line.split
-          [direction, steps.to_i]
-        end
-      end
-
-    calculate_lagoon_square(digging_sequence)
+    calculate_lagoon_area(
+      enable_part_two ? digging_sequence_part_two : digging_sequence_part_one
+    )
   end
 
-  def calculate_lagoon_square(digging_sequence)
-    lagoon = Lagoon.new
-    tranch_size = 0
+  def digging_sequence_part_one
+    @input.map do |line|
+      direction, steps, = line.split
+      [direction, steps.to_i]
+    end
+  end
 
-    digging_sequence.each do |direction, steps|
+  def digging_sequence_part_two
+    @input.map do |line|
+      _, _, hex_number = line.split
+      match = hex_number.match(/\(\#(?<steps>[0-9a-f]{5})(?<direction>[0-9a-f]{1})\)/)
+      [Lagoon::DIRECTIONS[match[:direction].to_i(16)].first, match[:steps].to_i(16)]
+    end
+  end
+
+  def calculate_lagoon_area(digging_sequence)
+    lagoon = Lagoon.new
+
+    tranch_size = digging_sequence.sum do |direction, steps|
       lagoon.digging(steps, direction)
-      tranch_size += steps
+      steps
     end
 
-    path_row_coordinates, path_col_coordinates = lagoon.path.transpose
-
-    ((
-      path_col_coordinates[0..-2].zip(path_row_coordinates[1..]).sum { |row, col| row * col } -
-      path_row_coordinates[0..-2].zip(path_col_coordinates[1..]).sum { |col, row| row * col }
-    ).abs / 2) + (tranch_size / 2) + 1
+    xn, yn = lagoon.path.transpose
+    ((xn[0..-2].zip(yn[1..]).sum { |x, y| x * y } - yn[0..-2].zip(xn[1..]).sum { |y, x| x * y }).abs / 2) +
+      (tranch_size / 2) + 1
   end
 end
